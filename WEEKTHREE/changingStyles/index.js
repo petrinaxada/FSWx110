3c003cad1b54b761096250ba458385c213c3ac5c
trinaxada <petrina.ada@bryanuniversity.edu>
2021-08-31 16:54:51 -0700

    changeStylesUpdated
    Replace Files with updated files via upload

--- WEEKTHREE/changingStyles/index.js
+++
@@ -1,9 +0,0 @@
-for (i = 0; i < 5; i++)
-{
-    let headers = document.createElement("h1");
-    headers.textContent = "How much does a polar bear weigh? ... enough to break the ice!";
-    document.body.appendChild(headers).style.fontSize = "20px"
-    document.body.appendChild(headers).style.fontWeight = "lighter"
-    document.body.appendChild(headers).style.fontFamily = "sans-serif"
-    document.body.appendChild(headers).style.color = "cornflowerblue"
-}
