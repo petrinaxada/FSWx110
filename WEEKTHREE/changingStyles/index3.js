a4ae3c7fda66dbbc23756ab334094d161d0e0617
trinaxada <petrina.ada@bryanuniversity.edu>
2021-08-31 16:57:53 -0700

    StyleChange

---
+++ WEEKTHREE/changingStyles/index3.js
@@ -0,0 +1,10 @@
+for (i = 0; i < 5; i++)
+{
+    let headers = document.createElement("h1");
+    headers.textContent = "Lorem Ipsum";
+    headers.classList.add("border");
+    document.body.appendChild(headers).style.fontSize = "20px"
+    document.body.appendChild(headers).style.fontWeight = "lighter"
+    document.body.appendChild(headers).style.fontFamily = "sans-serif"
+    document.body.appendChild(headers).style.color = "cornflowerblue"
+}
