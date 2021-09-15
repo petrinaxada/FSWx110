93d115552b8395e354bf45e82c7d85313a393a25
trinaxada <petrina.ada@bryanuniversity.edu>
2021-09-14 19:01:13 -0700

    Fix Code

--- WEEKSIX/MouseMove/app6.js
+++ WEEKSIX/MouseMove/app6.js
@@ -1,16 +1,11 @@
-var box = document.querySelector(".red-box");
+let boxNode = document.getElementsByClassName("red-box");
+let boxArr = Array.from(boxNode);
+let myBox = boxArr[0];
 
-var newPara = document.createElement("P");
-newPara.setAttribute("id","Para");
-newPara.innerText= "Coordinates";
-document.body.append(newPara);
+myBox.addEventListener("mousemove", function(e) {
+    myBox.textContent = "This is the mouse coordinates: X=" + e.clientX + " / Y=" + e.clientY;
+});
 
-var getPara = document.getElementById("Para");
-getPara.style.textAlign = "center";
-
-box.addEventListener("mouseover", XYZ);
-function XYZ(event){
-    var x = event.clientX;
-    var y = event.clientY;
-    getPara.innerHTML="Mouse Coordinates: " + x +" "+ y ;
-};
+myBox.addEventListener("mouseout", function() {
+    myBox.textContent = "";
+});
